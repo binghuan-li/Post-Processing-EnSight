a1e4c9df75e1416643ea9b5fe546fa659907d8d1
Binghuan Li <lbinghuan@outlook.com>
2025-01-27 19:42:45 +0000

    Update retrieve_vars.py

--- retrieve_vars.py
+++ retrieve_vars.py
@@ -1,4 +1,6 @@
 '''
+ws_utils.py
+
 load this script in ANSYS EnSight to calcuate the haemodynamic variables.
 
 Notes:
@@ -11,71 +13,84 @@ BWL, 02/10/2024
 # case specific global variables to modify
 #-----------------START HERE-----------------
 start_pt = 1;
-end_pt = 99;
-wall_part_ID = 6;
+end_pt = 48;
+wall_part_ID = 2;
 vol_part_ID = 1;
+
+var_names = {
+    "wss": "WALL_SHEAR_STRESS"
+};
+to_node = True;
+
 #-----------------UNTIL HERE-----------------
 
 print('Calculation begins')
 
 # calculate TAWSS
-ensight.variables.activate("Wall_Shear")
-ensight.variables.evaluate("rms_wall_shear = RMS(Wall_Shear)")
-ensight.part.select_begin(wall_part_ID)
-expr = "TAWSS = TempMean(plist,rms_wall_shear,{},{})".format(start_pt, end_pt)
-err = ensight.variables.evaluate(expr)
+if to_node:
+    ensight.variables.activate(f"{var_names['wss']}");
+    ensight.variables.evaluate(f"user_WALL_SHEAR_STRESS = ElemToNode(plist,{var_names['wss']})");
+    var_names['wss'] = "user_WALL_SHEAR_STRESS";
+
+ensight.variables.activate(f"{var_names['wss']}");
+ensight.variables.evaluate(f"user_rms_wall_shear = RMS({var_names['wss']})");
+ensight.part.select_begin(wall_part_ID);
+expr = "user_TAWSS = TempMean(plist,user_rms_wall_shear,{},{})".format(start_pt, end_pt);
+err = ensight.variables.evaluate(expr);
 if err == 0:
-    print('--> TAWSS calculation successful!')
+    print("\t--> TAWSS calculation successful!");
+else:
+    raise Exception("Calculation in TAWSS failed.");
 
 # calculate OSI
-expr = "TempMean_wss = TempMean(plist,Wall_Shear,{},{})".format(start_pt, end_pt)
-ensight.variables.evaluate(expr)
-ensight.part.select_begin(wall_part_ID)
-ensight.variables.evaluate("rms_tempmean_wss = RMS(TempMean_wss)")
-ensight.part.select_begin(wall_part_ID)
-err = ensight.variables.evaluate("OSI = 0.5 * ( 1 - (rms_tempmean_wss/TAWSS))")
-if err == 0:
-    print('--> OSI calculation successful!')
+# expr = "TempMean_wss = TempMean(plist,Wall_Shear,{},{})".format(start_pt, end_pt)
+# ensight.variables.evaluate(expr)
+# ensight.part.select_begin(wall_part_ID)
+# ensight.variables.evaluate("rms_tempmean_wss = RMS(TempMean_wss)")
+# ensight.part.select_begin(wall_part_ID)
+# err = ensight.variables.evaluate("OSI = 0.5 * ( 1 - (rms_tempmean_wss/TAWSS))")
+# if err == 0:
+    # print('--> OSI calculation successful!')
 
 # calculate ECAP
-ensight.part.select_begin(wall_part_ID)
-err = ensight.variables.evaluate("ECAP = OSI/TAWSS")
-if err == 0:
-    print('--> ECAP calculation successful!')
+# ensight.part.select_begin(wall_part_ID)
+# err = ensight.variables.evaluate("ECAP = OSI/TAWSS")
+# if err == 0:
+    # print('--> ECAP calculation successful!')
 
 # calculate RRT
-ensight.part.select_begin(wall_part_ID)
-err = ensight.variables.evaluate("RRT = 1/((1 - 2*OSI) *TAWSS)")
-if err == 0:
-    print('--> RRT calculation successful!')
+# ensight.part.select_begin(wall_part_ID)
+# err = ensight.variables.evaluate("RRT = 1/((1 - 2*OSI) *TAWSS)")
+# if err == 0:
+    # print('--> RRT calculation successful!')
 
 # calculate TransWSS
-ensight.part.select_begin(wall_part_ID)
-ensight.variables.evaluate("transwss_inner_frac = TempMean_wss/rms_wall_shear")
-ensight.part.select_begin(wall_part_ID)
-ensight.variables.evaluate("transwss_norm_of_frac = NormVect(plist,transwss_inner_frac)")
-ensight.part.select_begin(wall_part_ID)
-ensight.variables.evaluate("transwss_rms_of_norm = ABS(DOT(Wall_Shear, transwss_norm_of_frac))")
-ensight.part.select_begin(wall_part_ID)
-expr = "TransWSS = TempMean(plist,transwss_rms_of_norm,{},{})".format(start_pt, end_pt)
-err = ensight.variables.evaluate(expr)
-if err == 0:
-    print('--> TransWSS calculation successful!')
+# ensight.part.select_begin(wall_part_ID)
+# ensight.variables.evaluate("transwss_inner_frac = TempMean_wss/rms_wall_shear")
+# ensight.part.select_begin(wall_part_ID)
+# ensight.variables.evaluate("transwss_norm_of_frac = NormVect(plist,transwss_inner_frac)")
+# ensight.part.select_begin(wall_part_ID)
+# ensight.variables.evaluate("transwss_rms_of_norm = ABS(DOT(Wall_Shear, transwss_norm_of_frac))")
+# ensight.part.select_begin(wall_part_ID)
+# expr = "TransWSS = TempMean(plist,transwss_rms_of_norm,{},{})".format(start_pt, end_pt)
+# err = ensight.variables.evaluate(expr)
+# if err == 0:
+    # print('--> TransWSS calculation successful!')
 
 # calculate cross flow index
-ensight.part.select_begin(wall_part_ID)
-ensight.variables.evaluate("CFI_integrand = (Wall_Shear/RMS(Wall_Shear))* transwss_norm_of_frac")
-ensight.part.select_begin(wall_part_ID)
-expr = "CFI = TempMean(plist,CFI_integrand,{},{})".format(start_pt, end_pt)
-err = ensight.variables.evaluate(expr)
-if err == 0:
-    print('--> CFI calculation successful!')
+# ensight.part.select_begin(wall_part_ID)
+# ensight.variables.evaluate("CFI_integrand = (Wall_Shear/RMS(Wall_Shear))* transwss_norm_of_frac")
+# ensight.part.select_begin(wall_part_ID)
+# expr = "CFI = TempMean(plist,CFI_integrand,{},{})".format(start_pt, end_pt)
+# err = ensight.variables.evaluate(expr)
+# if err == 0:
+    # print('--> CFI calculation successful!')
 
 # calculate turbulence intensity
-ensight.part.select_begin(vol_part_ID)
-ensight.variables.activate("Turbulence_Kinetic_Energy")
-err = ensight.variables.evaluate("TI = SQRT((2/3) * Turbulence_Kinetic_Energy)/Velocity")
-if err == 0:
-    print('--> TI calculation successful!')
+# ensight.part.select_begin(vol_part_ID)
+# ensight.variables.activate("Turbulence_Kinetic_Energy")
+# err = ensight.variables.evaluate("TI = SQRT((2/3) * Turbulence_Kinetic_Energy)/Velocity")
+# if err == 0:
+    # print('--> TI calculation successful!')
 
 print('Done')
