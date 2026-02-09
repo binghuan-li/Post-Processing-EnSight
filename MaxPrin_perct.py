75fcf7446089a923637dc8ee8759132f127e10a5
B-H Li <72060034+binghuan-li@users.noreply.github.com>
2026-02-09 17:35:18 +0000

    Add percentile utilities

---
+++ MaxPrin_perct.py
@@ -0,0 +1,28 @@
+'''
+MaxPrin_perct.py
+
+load this script in ANSYS EnSight to calcuate the 99 percentile of the max principal stress.
+(or other scalar quantities)
+
+BWL, 09/02/2026
+'''
+
+import numpy as np
+
+#-----------------MODIFICATION START HERE-----------------
+# 'wall':       <part name in EnSight>
+# 'stress':     <variable name in EnSight>
+setup = {
+            'wall': 'Part 0', 
+            'stress': 'Stress_maxprin_el'
+        }
+#-----------------MODIFICATION UNTIL HERE-----------------
+
+ensight.variables.activate(setup['stress'])
+ensight.solution_time.update_to_current()
+
+part = ensight.objs.core.PARTS[setup['wall']][0]
+coords = ensight.objs.core.VARIABLES['Coordinates'][0]
+ws = part.get_values([coords,  setup['stress']], activate=1)
+ws = ws[setup['stress']]
+print(f'\t --->The 99 percentile of max principle stress is {np.percentile(ws, 0.99)}')
---
+++ WSS_perct.py
@@ -0,0 +1,28 @@
+'''
+WSS_perct.py
+
+load this script in ANSYS EnSight to calcuate the 99 percentile of the wall shear stress.
+(or other vector quantities)
+
+BWL, 09/02/2026
+'''
+
+import numpy as np
+
+#-----------------MODIFICATION START HERE-----------------
+# 'wall':       <part name in EnSight>
+# 'wall_shear': <variable name in EnSight>
+setup = {
+            'wall': 'wall', 
+            'wall_shear': 'Wall_Shear'
+        }
+#-----------------MODIFICATION UNTIL HERE-----------------
+
+ensight.variables.activate(setup['wall_shear'])
+ensight.solution_time.update_to_current()
+
+part = ensight.objs.core.PARTS[setup['wall']][0]
+coords = ensight.objs.core.VARIABLES['Coordinates'][0]
+wss = part.get_values([coords,  setup['wall_shear']], activate=1) # returned as a dict
+wss = np.linalg.norm(wss[setup['wall_shear']], axis=1) # unpack and calc the mag
+print(f'\t --->The 99 percentile of wss is {np.percentile(wss, 0.99)}')
